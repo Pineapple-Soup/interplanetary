8bc37899d693559482ec26acb649b697f02102a7
zenxha <komays5042@gmail.com>
2024-11-16 14:33:46 -0800

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#17153B', // Darkest (background)
+        secondary: '#C8ACD6', // Lighter (text)
+        tertiary: "#2E236C", // Dark
+        accent: '2E236C', // Accent color
+        highlight: '#718096', // Highlight color
+        blueish: '#7e9cd6', // Off-white color
+      },
+    },
   },
   plugins: [],
 }
