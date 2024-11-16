1c5870ad64618796b945df62797743e289bfcfb5
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-11-16 11:56:05 -0800

    build: Create SEPHI class

---
+++ src/sephi.py
@@ -0,0 +1,9 @@
+
+class SEPHI:
+    def __init__(self, planet_mass, planet_radius, stellar_mass, stellar_radius, stellar_effective_temperature, planetary_system_age) -> None:
+        self.planet_mass = planet_mass
+        self.planet_radius = planet_radius
+        self. stellar_mass = stellar_mass
+        self.stellar_radius = stellar_radius
+        self.stellar_effective_temperature = stellar_effective_temperature
+        self.planetary_system_age = planetary_system_age
