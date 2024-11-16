c0121b5a49eb6c0a9876767ef5f6c0e1b19dd605
Kolzza <cole.miller2022@gmail.com>
2024-11-16 13:05:12 -0800

    add L4

--- src/sephi.py
+++ src/sephi.py
@@ -1,10 +1,8 @@
 import constants
 import math
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 18aabc29d1178f3bd37c01643917d69981c48dbd
+
 class SEPHI:
     def __init__(self, planet_mass, planet_radius, stellar_mass, stellar_radius, stellar_effective_temperature, planetary_system_age, angular_frequency) -> None:
         self.planet_mass = planet_mass
@@ -13,18 +11,8 @@ class SEPHI:
         self.stellar_radius = stellar_radius
         self.stellar_effective_temperature = stellar_effective_temperature
         self.planetary_system_age = planetary_system_age
-<<<<<<< HEAD
         self.angular_frequency = angular_frequency
-        relative_mass = planet_mass / constants.EARTH_MASS
 
-
-    def calculate_L4(self):
-        density = self.relative_mass / ((4 / 3 * math.pi) * (self.planet_radius ** 3))
-        density = density ** (1/2)
-        mag_radius = self.planet_radius ** (7 / 2)
-        mag_field = density * mag_radius * self.angular_frequency
-        return mag_field
-=======
     
     def get_relative_mass(self):
         return self.planet_mass / constants.EARTH_MASS
@@ -45,4 +33,10 @@ class SEPHI:
             return 0
         else:
             return math.exp(-0.5 * ((relative_radius - mu1)/sigma1)**2)
->>>>>>> 18aabc29d1178f3bd37c01643917d69981c48dbd
+
+    def calculate_L4(self):
+        density = self.get_relative_mass() / ((4 / 3 * math.pi) * (self.planet_radius ** 3))
+        density = density ** (1/2)
+        mag_radius = self.planet_radius ** (7 / 2)
+        mag_field = density * mag_radius * self.angular_frequency
+        return mag_field
